d059ae5256334787993e79f53883a21989f9711c
Sofa <mezasdieg@myvuw.ac.nz>
2023-03-25 17:49:27 +1300

    Block subclass that can be drawn

---
+++ src/ColorBlock.java
@@ -0,0 +1,22 @@
+import java.awt.*;
+import ecs100.*;
+public class ColorBlock extends Block{
+    private Color color;
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    ColorBlock(double x, double y, double width, double height, Color color) {
+        super(x, y, width, height);
+        this.color = color;
+    }
+
+    public void draw() {
+        UI.setColor(getColor());
+        UI.fillRect(getX(), getY(), getWidth(), getHeight());
+    }
+}
