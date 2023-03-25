848b22c05dd0f8de70f3fcf5aea0359b00fdc964
Sofa <mezasdieg@myvuw.ac.nz>
2023-03-25 18:09:37 +1300

    Handle player in Game class, set up GUI and game loop in Main class

--- src/Game.java
+++ src/Game.java
@@ -1,8 +1,28 @@
 import ecs100.*;
 public class Game {
-    public static void main(String[] args) {
-        // Read Map Data
-        // Create Block Objects
+    public Player player;
+    private boolean done = false;
 
+    public boolean getDone() {
+        return done;
     }
+
+    public void handleMovement(String key) {
+
+        // We can treat it as a char for easier handling
+        if (key.length() == 1) {
+            char k = key.charAt(0);
+
+            switch (k) {
+                case 'd' -> player.moveRight();
+                case 'a' -> player.moveLeft();
+                case 'q' -> done = true;
+            }
+        }
+    }
+
+    Game() {
+        player = new Player(20, 20);
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class Main {
+import ecs100.UI;
+
+public class Main {
+    public static void setupGUI(Game game) {
+        UI.initialise();
+        UI.setKeyListener(game::handleMovement);
+    }
+
+    public static void main(String[] args) {
+        Game game = new Game();
+        setupGUI(game);
+        UI.println("Hi!");
+
+        final double frame_len = 1.0/1.0;
+        while (!game.getDone()) {
+            UI.clearGraphics();
+            game.player.draw();
+            UI.sleep(frame_len);
+        }
+    }
 }
